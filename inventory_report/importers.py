f8a2390bf9bfc84253b2576a835c6818a03db76b
mikael <mikaeljeffeson@gmail.com>
2023-09-28 09:39:31 -0300

    Feat: Req 1,3,4,5

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,12 +1,41 @@
 from typing import Dict, Type
+from abc import abstractmethod
+from inventory_report.product import Product
+import json
+from typing import List
 
 
 class Importer:
-    pass
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        if not self.path.endswith('.json'):
+            raise ValueError("Invalid file format")
+
+        with open(self.path, 'r') as file:
+            data = json.load(file)
+
+        response = [
+            Product(
+                id=product['id'],
+                product_name=product['product_name'],
+                company_name=product['company_name'],
+                manufacturing_date=product['manufacturing_date'],
+                expiration_date=product['expiration_date'],
+                serial_number=product['serial_number'],
+                storage_instructions=product['storage_instructions'],
+            )
+            for product in data
+        ]
+
+        return response
 
 
 class CsvImporter:
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="22",
+        company_name="xesque",
+        product_name="dele",
+        manufacturing_date="2022-09-11",
+        expiration_date="2024-06-20",
+        serial_number="MMMM112",
+        storage_instructions="cuidado produto fragil",
+    )
+
+    assert product.id == "22"
+    assert product.company_name == "xesque"
+    assert product.product_name == "dele"
+    assert product.manufacturing_date == "2022-09-11"
+    assert product.expiration_date == "2024-06-20"
+    assert product.serial_number == "MMMM112"
+    assert product.storage_instructions == "cuidado produto fragil"
