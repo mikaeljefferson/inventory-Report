998fc6d503146af9bacaa6599456b92dac8c5c1b
mikael <mikaeljeffeson@gmail.com>
2023-10-02 08:45:17 -0300

    Feat: Req 2,6

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "22",
+        "xesque",
+        "dele",
+        "2022-09-11",
+        "2024-06-20",
+        "MMMM112",
+        "cuidado produto fragil",
+    )
+    assert product.__str__() == (
+        f"The product {product.id} - {product.product_name} "
+        f"with serial number {product.serial_number} "
+        f"manufactured on {product.manufacturing_date} "
+        f"by the company {product.company_name} "
+        f"valid until {product.expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{product.storage_instructions}."
+    )
